52921b31a42ab4783796847ec244a60f91744e1f
Abdulrahman Amir <64253660+3bdulra7manAmir@users.noreply.github.com>
2022-12-30 17:20:51 +0200

    CreatingDBConnection_AbdulrahmanAmir

--- Mobile Repairs Management System/Functions.cs
+++ Mobile Repairs Management System/Functions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,22 @@ namespace Mobile_Repairs_Management_System
 {
     class Functions
     {
+        private SqlConnection Con;
+        private SqlCommand Cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private string ConStr;
+
+        //DB connection
+        public Functions()
+        {
+            ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ethan\OneDrive\Documents\Visual Studio 2019\Code\MRsMS\DataBase\MobileRepairOb.mdf;Integrated Security=True;Connect Timeout=30";
+            Con = new SqlConnection(ConStr);
+            Cmd = new SqlCommand();
+            Cmd.Connection = Con;
+        }
+
+
 
     }
 }
