40a7fc8b35395d93b01d773e2ebafc5182fd2a08
Abdulrahman Amir <64253660+3bdulra7manAmir@users.noreply.github.com>
2022-12-30 17:14:10 +0200

    CreatingDBFunctions_AbdulrahmanAmir

---
+++ Mobile Repairs Management System/Functions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile_Repairs_Management_System
+{
+    class Functions
+    {
+
+    }
+}
