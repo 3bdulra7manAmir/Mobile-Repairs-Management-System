69218e23cb51e9216df2ac375773eccc224f7830
Abdulrahman Amir <64253660+3bdulra7manAmir@users.noreply.github.com>
2022-12-30 19:03:31 +0200

    CustomersSaveBtn_AbdulrahmanAmir

--- Mobile Repairs Management System/Customers.cs
+++ Mobile Repairs Management System/Customers.cs
@@ -17,6 +17,7 @@ namespace Mobile_Repairs_Management_System
         {
             InitializeComponent();
             Con = new Functions();
+            ShowCustomers();
         }
 
         private void ShowCustomers()
@@ -27,7 +28,28 @@ namespace Mobile_Repairs_Management_System
 
         private void SaveBtn_Click(object sender, EventArgs e)
         {
-
+            if(CustNameTb.Text == "" || CustPhoneTb.Text == "" || CustAddTb.Text == "")
+            {
+                MessageBox.Show("MissingData!!");
+            }
+            else
+            {
+                try
+                {
+                    string CName = CustNameTb.Text;
+                    string CPhone = CustPhoneTb.Text;
+                    string CAdd = CustAddTb.Text;
+                    string Query = "insert into CustomerTb1 values('{1}','{2}','{3}')";
+                    Query = string.Format(Query, CName, CPhone, CAdd);
+                    int i = Con.SetData(Query);
+                    MessageBox.Show("Customer Added!");
+                    ShowCustomers();
+                }
+                catch (Exception Ex)
+                {
+                    MessageBox.Show(Ex.Message);
+                }
+            }
         }
     }
 }
